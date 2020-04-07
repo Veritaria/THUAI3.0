d8666d9d39c85e1323d2b6879cffb3bf03c62a8c
Li Moufan <veritaria@outlook.com>
2020-04-07 18:55:31 +0800

    actions里编译CAPI

---
+++ CAPI/compile_actions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ./CAPI
+cat src/player.cpp | grep "#define DEVELOPER_ONLY"
+if [ $? -ne 1 ];then
+	echo "file inclusion error" >> error.txt
+	exit 1
+fi
+mkdir build
+cd build
+cmake ..
+make >error.txt 2>&1
